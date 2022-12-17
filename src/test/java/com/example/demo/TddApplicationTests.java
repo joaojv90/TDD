f07404a81a23fa1b7d123bdb0e88730c1ae29cdb
joaojv90 <joao.jaramillo@gmail.com>
2022-12-16 19:58:20 -0500

    deleted comments and unnecessary lines

--- src/test/java/com/example/demo/TddApplicationTests.java
+++ src/test/java/com/example/demo/TddApplicationTests.java
@@ -10,17 +10,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TddApplicationTests {
 	
-//	Write a method that will receive a list of numbers and this method will return the text:
-//		- "FizzBuzz" if i is divisible by 3 and 5.
-//		- "Fizz" if i is divisible by 3.
-//		- "Buzz" if i is divisible by 5.
-//		- If none of the above conditions are true, then return the given number
-
-	//@Test
-	void contextLoads() {	
-		
-	}
-	
 	public String checkNum (int [] nums) {
 		if (nums[0]%3==0 && nums[0]%5==0) {
 			return "FizzBuzz";
